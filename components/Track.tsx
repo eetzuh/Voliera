18be75caeeb4a93dc0f9c0a43ed3f592020110a5
Irena Kandić <irenakandic9@gmail.com>
2024-10-05 23:09:28 +0200

    Reading metadata fast through individual track reading

--- components/Track.tsx
+++ components/Track.tsx
@@ -1,15 +1,50 @@
 import { View, Text, Image } from 'react-native'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import * as FileSystem from 'expo-file-system';
+import parse from 'id3-parser'
+import { toByteArray } from 'react-native-quick-base64';
 
-const Track = ({albumImage}:{albumImage: string | null}) => {
-  return (
-    <View style={{width:"100%", height:60, backgroundColor:'#76738160'}}> 
-    {albumImage !== null ?
-        <Image source={{uri:`data:image/jpeg;base64,${albumImage}` }}
-        style={{ width: 200, height: 200, borderWidth:2 }}/>
-    :
-    <Text>asd</Text>
+const Track = ({ uri, duration }: { uri: string, duration:number }) => {
+  interface ID3Structure {
+      artist: string | null;
+      album: string | null;
+      title: string | null;
+      year: string | null;
+      image: string | null;
+  }
+
+  const [artwork, setArtwork] = useState<string | null>(null)
+  const [songInfo, setSongInfo] = useState<ID3Structure>()
+
+  function arrayBufferToBase64(buffer: ArrayLike<number>) {
+    let binary = '';
+    const bytes = new Uint8Array(buffer);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
     }
+    return window.btoa(binary);
+}
+  async function getSongInfo(uri: string) {
+    const readFileInfo = await FileSystem.readAsStringAsync(uri, { encoding: FileSystem.EncodingType.Base64 });
+    const ID3Tags = parse(toByteArray(readFileInfo))
+     if (ID3Tags && ID3Tags.image && ID3Tags.image.data) {
+      const base64Image = arrayBufferToBase64(ID3Tags.image.data);
+      setArtwork(base64Image)
+    }
+  }
+  useEffect(()=>{
+    getSongInfo(uri)
+  },[])
+
+  return (
+    <View style={{ width: "100%", height: 65, backgroundColor: '#76738160', justifyContent:'center', paddingHorizontal:20 }}>
+      {artwork !== null ?
+        <Image source={{ uri: `data:image/jpeg;base64,${artwork}` }}
+          style={{ width: 60, height: 60, borderWidth: 2, borderRadius:10 }} />
+        :
+        <Text>nema</Text>
+      }
     </View>
   )
 }
--- components/screens/SongsScreen.tsx
+++ components/screens/SongsScreen.tsx
@@ -1,73 +1,25 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useContext } from 'react'
+import { View, Text } from 'react-native'
 import { styles } from '../../styles/AppStyles'
 import { ThemeContext, useTracks } from '../../context/Context'
-import * as FileSystem from 'expo-file-system';
-import Track from '../Track'
-import parse from 'id3-parser'
+import Track from '../Track';
 
 const SongsScreen = () => {
-    const [albumImages, setAlbumImages] = useState<string[]>([])
     const theme = useContext(ThemeContext);
     const { tracks } = useTracks();
-
-    function arrayBufferToBase64(buffer: ArrayLike<number>) {
-        let binary = '';
-        const bytes = new Uint8Array(buffer);
-        const len = bytes.byteLength;
-        for (let i = 0; i < len; i++) {
-            binary += String.fromCharCode(bytes[i]);
-        }
-        return window.btoa(binary);
-    }
-
-    async function getSongInfo() {
-        const ID3Tracks: string[] = [];
-        try {
-            if (tracks) {
-                for (const track of tracks) {
-                    console.log("pocelo")
-                    const readFileInfo = await FileSystem.readAsStringAsync(track.uri, { encoding: FileSystem.EncodingType.Base64 });
-                    const binaryString = atob(readFileInfo);
-                    const uint8Array = new Uint8Array(
-                        binaryString.split('').map(char => char.charCodeAt(0))
-                    );
-                    const ID3Tags = parse(uint8Array)
-                    if (ID3Tags && ID3Tags.image && ID3Tags.image.data) {
-                        console.log(ID3Tags.title)
-                        const imageBinary = ID3Tags.image.data;
-                        const base64Image = arrayBufferToBase64(imageBinary);
-                        ID3Tracks.push(base64Image)
-                    }
-                }
-                console.log("tusam")
-            setAlbumImages(ID3Tracks);
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    useEffect(() => {        
-    getSongInfo();
-    }, [])
-
     return (
         <View style={[styles.container, { backgroundColor: theme.bgColorPrimay }]}>
             <View style={{ height: 50, width: "100%", borderBottomWidth: 1, borderColor: theme.colorLight }}></View>
             <Text>Songs...</Text>
-            {tracks &&
-                albumImages.map((image, index) => {
-                    return (
-                        <Track key={"track"+index} albumImage={image}></Track>
-                    )
-                })
-            }
-            {/* <TouchableOpacity style={{ backgroundColor: 'red', width: 50, alignSelf: "center" }} onPress={getSongInfo}>
-                <Text>
-                    PLAY
-                </Text>
-            </TouchableOpacity> */}
+            <View style={{gap:6}}>
+                {tracks &&
+                    tracks.map((track, index) => {
+                        return (
+                            <Track key={"track" + index} uri={track.uri} duration={track.duration}/>
+                        )
+                    })
+                }
+            </View>
         </View>
     )
 }
